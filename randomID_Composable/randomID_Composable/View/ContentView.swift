35ff0a45ae239859b66b68d451545acf8cf2ea82
chanwoo <humains29@gmail.com>
2021-07-13 18:50:36 +0900

    :sparkles: Install Composable Architecture

--- randomID_Composable/randomID_Composable/View/ContentView.swift
+++ randomID_Composable/randomID_Composable/View/ContentView.swift
@@ -4,6 +4,8 @@ struct ContentView: View {
   @State var idText: String = "ID"
   @State var titleText: String = "TITLE"
   
+  @ObservedObject var viewModel = ContentViewModel()
+  
   var body: some View {
     VStack {
       Text(idText)
@@ -14,8 +16,14 @@ struct ContentView: View {
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
-}
+
+
+
+
+
+
+//struct ContentView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    ContentView()
+//  }
+//}
--- randomID_Composable/randomID_Composable/ViewModel/ContentViewModel.swift
+++ randomID_Composable/randomID_Composable/ViewModel/ContentViewModel.swift
@@ -1,6 +1,19 @@
 import SwiftUI
 import Combine
+import ComposableArchitecture
 
 class ContentViewModel: ObservableObject {
+  let didChange = PassthroughSubject<Void, Never>()
+  private var cancellables: [AnyCancellable] = []
 
+  enum Input {
+    case viewDidAppear
+    case buttonTap
+  }
+  
+  func fetchRandomDate() {
+    FetchRandomInfo.shared.fetchData(completion: {_ in
+      // 데이터 변경
+    })
+  }
 }
